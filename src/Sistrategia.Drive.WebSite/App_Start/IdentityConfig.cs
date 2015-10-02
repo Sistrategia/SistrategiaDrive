746b4934512e53c83e510f7daaf9e68270a2bce2
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-10-02 10:22:11 -0500

    Welcome banner change and security settings.

--- src/Sistrategia.Drive.WebSite/App_Start/IdentityConfig.cs
+++ src/Sistrategia.Drive.WebSite/App_Start/IdentityConfig.cs
@@ -24,25 +24,25 @@ namespace Sistrategia.Drive.WebSite
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) {
             var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
 
-            //// Configure validation logic for usernames
-            //manager.UserValidator = new UserValidator<ApplicationUser>(manager) {
-            //    AllowOnlyAlphanumericUserNames = false,
-            //    RequireUniqueEmail = true
-            //};
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager) {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
 
-            //// Configure validation logic for passwords
-            //manager.PasswordValidator = new PasswordValidator {
-            //    RequiredLength = 6,
-            //    RequireNonLetterOrDigit = false, //true,
-            //    RequireDigit = true,
-            //    RequireLowercase = true,
-            //    RequireUppercase = true,
-            //};
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false, //true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
 
-            //// Configure user lockout defaults
-            //manager.UserLockoutEnabledByDefault = true;
-            //manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            //manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
 
             //// Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             //// You can write your own provider and plug it in here.
@@ -55,11 +55,12 @@ namespace Sistrategia.Drive.WebSite
             //});
             //manager.EmailService = new EmailService();
             //manager.SmsService = new SmsService();
-            //var dataProtectionProvider = options.DataProtectionProvider;
-            //if (dataProtectionProvider != null) {
-            //    manager.UserTokenProvider =
-            //        new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
-            //}
+
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null) {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
 
             return manager;
         }
